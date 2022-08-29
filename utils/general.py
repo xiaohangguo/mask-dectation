93fa630375ddcddc902d17c640ca72dd07c38864
glennjocher <glenn.jocher@ultralytics.com>
2022-08-29 15:16:09 +0200

    check remote ultralytics/HUB

--- utils/general.py
+++ utils/general.py
@@ -300,7 +300,7 @@ def git_describe(path=ROOT):  # path must be a directory
 
 @TryExcept()
 @WorkingDirectory(ROOT)
-def check_git_status(repo='ultralytics/yolov5', branch='master'):
+def check_git_status(repo='ultralytics/yolov5', branch='ultralytics/HUB'):
     # YOLOv5 status check, recommend 'git pull' if code is out of date
     url = f'https://github.com/{repo}'
     msg = f', for updates see {url}'
